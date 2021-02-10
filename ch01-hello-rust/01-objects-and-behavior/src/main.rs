6590f3f675780af13adfeb3d8eccacd343302f93
francoposa <franco@francoposa.io>
2021-02-10 10:16:10 -0600

    basic trait definition, implementation, and test

---
+++ ch01-hello-rust/01-objects-and-behavior/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {}
+
+struct Door {
+    is_open: bool,
+}
+
+impl Door {
+    fn new(is_open: bool) -> Self {
+        Self { is_open }
+    }
+}
+
+trait Openable {
+    fn open(&mut self);
+}
+
+impl Openable for Door {
+    fn open(&mut self) {
+        self.is_open = true;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn open_door() {
+        let mut door = Door::new(false);
+        door.open();
+        assert!(door.is_open);
+    }
+}
